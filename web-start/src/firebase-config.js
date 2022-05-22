d50344f6d121af58d3d9b8a2dfac6f8abe3527b1
mig <michaelleojacob@gmail.com>
2022-05-22 09:27:41 -0700

    initial commit

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -1,20 +1,32 @@
+import { initializeApp } from 'firebase/app';
+
 /**
  * To find your Firebase config object:
- * 
+ *
  * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: 'AIzaSyAAfYBBUwQfVVg_VL9b9J6PWSERHQxXTJA',
+  authDomain: 'alpine-task-342916.firebaseapp.com',
+  projectId: 'alpine-task-342916',
+  storageBucket: 'alpine-task-342916.appspot.com',
+  messagingSenderId: '486022655273',
+  appId: '1:486022655273:web:ff27e7092ac061a270db38',
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+    throw new Error(
+      'No Firebase configuration object provided.' +
+        '\n' +
+        "Add your web app's configuration object to firebase-config.js"
+    );
   } else {
     return config;
   }
 }
+
+const app = initializeApp(config);
